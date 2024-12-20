8a55170cfe56b6408b169e8b1adf8146f2195437
Francisco Junior <jun.oliveira10@gmail.com>
2024-12-20 18:51:35 +0000

    Initial

--- src/main/java/com/matrix/view/kafka_stream_java/KafkaStreamJavaApplication.java
+++ src/main/java/com/matrix/view/kafka_stream_java/KafkaStreamJavaApplication.java
@@ -1,16 +1,25 @@
 package com.matrix.view.kafka_stream_java;
 
-import com.matrix.view.kafka_stream_java.config.KafkaConfig;
+import com.matrix.view.kafka_stream_java.service.KafkaStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackageClasses = KafkaConfig.class)
 public class KafkaStreamJavaApplication {
+
+	@Autowired
+	private KafkaStream kafkaStream;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaStreamJavaApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void initStreams() {
+		kafkaStream.initStreams();
 	}
 
 }
